e076a0f59db71db41adbad7434a8975f01468f0b
chenjinxinlove <505150704@qq.com>
2018-08-18 12:34:20 +0800

    rate test

---
+++ tests/unit/rate.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount, mount } from '@vue/test-utils'
+import Demo from '../../src/rate/demo'
+import Vue from 'vue'
+import Rate from '../../src/rate'
+
+describe('Rate.vue', () => {
+  // 测试是否可以被使用
+  it('use', () => {
+    Vue.use(Tag)
+    expect(Vue.component(Tag.name)).toHaveProperty('name', 'VueComponent')
+  })
+  // 测试能正常显示内容
+  it('props', () => {
+    const wrapper = shallowMount(Rate, {
+      propsData: {
+        type: 'primary',
+        plain: true
+      }
+    })
+    expect(wrapper.classes()).toEqual(['afo-tag--primaryplain', 'afo-tag'])
+  })
+  // demo测试快照
+  it('demo snapshot', () => {
+    expect(mount(Demo)).toMatchSnapshot()
+  })
+})
