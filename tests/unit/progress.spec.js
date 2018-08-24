177f69a2a81d34147b67fec99a37f342bd6271a8
陈金鑫 <505150704@qq.com>
2018-08-24 12:32:00 +0800

    progress test

--- tests/unit/progress.spec.js
+++ tests/unit/progress.spec.js
@@ -1,13 +1,13 @@
 import { mount } from '@vue/test-utils'
 import Demo from '../../src/pagination/demo'
 import Vue from 'vue'
-import Pagination from '../../src/pagination'
+import Progress from '../../src/pagination'
 
-describe('Pagination.vue', () => {
+describe('Progress.vue', () => {
   // 测试是否可以被使用
   it('use', () => {
-    Vue.use(Pagination)
-    expect(Vue.component(Pagination.name)).toHaveProperty('name', 'VueComponent')
+    Vue.use(Progress)
+    expect(Vue.component(Progress.name)).toHaveProperty('name', 'VueComponent')
   })
   // demo测试快照
   it('demo snapshot', () => {
