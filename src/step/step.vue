656a370458ecbf2275e36b7c4476a41fd63af816
chenjinxinlove <505150704@qq.com>
2018-08-25 20:53:34 +0800

    steps

---
+++ src/step/step.vue
@@ -0,0 +1,27 @@
+<template>
+  
+</template>
+<script>
+export default {
+  name: 'afo-step',
+  beforeCreate() {
+    this.$parent.steps.push(this)
+  },
+  computed: {
+    status () {
+      const index = this.$parent.steps.indexOf(index)
+      const active = this.$parent.active
+      if (index < active) {
+        return 'finish'
+      } else if (index === active) {
+        return 'process'
+      }
+    },
+    titleStyle () {
+      return this.status === 'process' ? {
+        color: this.$parent.activeColor
+      } : {}
+    }
+  }
+}
+</script>
