3c79834869c9febb101affb27455aa916e27b7a5
chenjinxinlove <505150704@qq.com>
2018-08-26 20:47:22 +0800

    steps

--- src/step/step.vue
+++ src/step/step.vue
@@ -25,3 +25,113 @@ export default {
   }
 }
 </script>
+<style lang="stylus">
+.afo-step {
+  flex: 1
+  font-size: 14px
+  position: relative
+  color: $gray-dark
+
+  &--horizontal
+    float: left
+
+    &:first-child
+      .afo-step__title
+        transform: none
+        margin-left: 0
+    &:last-child
+      position: absolute
+      right: 10px
+      width: auto
+
+      .afo-step__title
+        transform: none
+        margin-left: 0
+      .afo-step__circle-container
+        left: auto
+        right: -9px
+
+      .afo-step__line
+        width: 0
+    .afo-step__circle-container
+      position: absolute
+      top: 28px
+      left: -8px
+      padding: 0 8px
+      background-color: $white
+      z-index: 1
+    }
+
+    .afo-step__title
+      font-size: 12px
+      transform: translate3d(-50%, 0, 0)
+      display: inline-block
+      margin-left: 3px
+
+    .afo-step__line
+      position: absolute
+      left: 0
+      top: 30px
+      width: 100%
+      height: 1px
+      background-color: $border-color
+
+    &.afo-step--finish
+      color: $text-color
+
+      .afo-step__circle,
+      .afo-step__line
+        background-color: $green
+    &.afo-step--process
+      color: $text-color
+
+      .afo-step__circle-container
+        top: 24px
+      .afo-icon
+        font-size: 12px
+        color: $green
+        display: block
+  .afo-step__circle
+    display: block
+    width: 5px
+    height: 5px
+    background-color: #888
+    border-radius: 50%
+  &--vertical
+    float: none
+    display: block
+    font-size: 14px
+    line-height: 18px
+    padding: 10px 10px 10px 0
+
+    &:not(:last-child)::after
+      border-bottom-width: 1px
+    &:first-child
+      &::before
+        content: ''
+        position: absolute
+        width: 1px
+        height: 20px
+        background-color: $white
+        top: 0
+        left: -15px
+        z-index: 1
+    .afo-step__circle-container > i
+      position: absolute
+      z-index: 2
+    .afo-icon-checked
+      top: 12px
+      left: -20px
+      line-height: 1
+      font-size: 12px
+    .afo-step__circle
+      top: 16px;
+      left: -17px
+    .afo-step__line
+      position: absolute
+      top: 0
+      left: -15px
+      width: 1px
+      height: 100%
+      background-color: $border-color
+</style>
---
+++ src/steps/components/steps.vue
@@ -0,0 +1,69 @@
+<template>
+  
+</template>
+<script>
+export default {
+  name: 'steps',
+  props: {
+    icon: String,
+    title: String,
+    avtive: Number,
+    iconClass: String,
+    description: String,
+    direction: {
+      type: String,
+      default: 'horizontal'
+    },
+    activeColor: {
+      type: String,
+      default: '#06bf04'
+    }
+  },
+  data () {
+    return {
+      steps: []
+    }
+  }
+}
+</script>
+<style lang="stylus">
+.afo-steps
+  overflow: hidden
+  background-color: $white
+
+  &--horizontal {
+    padding: 0 10px
+
+    .afo-steps__items
+      display: flex
+      margin: 0 0 10px
+      overflow: hidden
+      position: relative
+      padding-bottom: 22px
+
+      &.afo-steps__items--alone
+        padding-top: 10px
+
+  &--vertical
+    padding: 0 0 0 35px
+
+  &__icon
+    float: left
+    margin-right: 10px
+
+  .afo-icon
+    font-size: 40px
+
+  &__message
+    height: 40px
+    margin: 15px 0
+  &__title
+    font-size: 14px
+    color: $text-color
+    padding-top: 4px
+
+  &__desc
+    font-size: 12px
+    line-height: 1.5
+    color: $gray-dark
+</style>
